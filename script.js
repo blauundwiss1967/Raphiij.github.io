b58afe8ba96788c1fc099298505ce78e403069ab
Raphiij <33808062+Raphiij@users.noreply.github.com>
2017-12-16 22:11:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+/*function myFunction() {
+  var Zahl=9999/3.145159;
+  var Pe=Zahl.toFixed(1);
+    document.getElementById("demo").innerHTML =Pe;}*/
+
+
+
+/*var zahl=prompt("Gib eine Zahl ein");
+
+   if (zahl < 0 || zahl >10 ) {
+
+      alert("Gib eine andere Zahl ein!");}
+
+
+    else if (zahl < 10 || zahl > 0) {
+
+      alert("Deine Zahl ist "+ zahl)}*/
+
+
+var today = new Date();
+var hournow = today.getHours();
+var greeting;
+
+ if (hournow > 18) {
+   greeting = 'Guten Abend!';
+
+ }
+ else if (hournow > 12) {greeting='Guten Tag!';
+
+ }
+ else if (hournow > 0) {greeting= 'Guten Morgen!'
+
+ }
+ else  {greeting='Willkommen!';
+ }
+
+ document.write('<p>' + greeting + '</p>');
